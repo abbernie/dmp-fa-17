072a82074200f573411e0c0f4b68e0cb97c72218
Andrew Bernstein <andrewbbernstein@gmail.com>
2017-07-23 01:10:26 -0400

    class day 2 updates

--- z-extras/animated logo/sketch.js
+++ z-extras/animated logo/sketch.js
@@ -1,24 +1,18 @@
-// This is a comment! It won't be rendered.
-
+// setup global variables
 var lines;
 var inc = 0;
+
 //Setup happens once when you run the sketch
 function setup() {
   //Create and size a canvas (w, h)
   createCanvas(300,300);
   lines = new Array();
 
-  for(var i = 0; i < 4; i++){
-    var myline = new MovingLine(random(displayWidth), random(displayHeight), 
-<<<<<<< HEAD
-      random(displayWidth), random(displayHeight));
-=======
-      random(displayWidth), random(displayHeight))
->>>>>>> master
+  for(var i = 0; i < 3; i++){
+    var myline = new MovingLine(random(50, 200), random(50,200), 
+      random(20,300), random(20,300));
     lines.push(myline);
   }
-
-  console.log(lines.length)
 
 }
 
@@ -30,20 +24,27 @@ function draw() {
     lines[i].draw();
   }
 
-  inc = (inc + 1)%100; 
+
+  inc = (inc + 1)%100;
+  console.log(inc);
 
 
   
 }
 
+// my moving line object
+
 var MovingLine = function(x1,x2,y1,y2){
+
+  this.red = random(255);
+  this.green = random(255);
+  this.blue = random(255);
 
   this.x1 = x1;
   this.y1 = x2;
   this.x2 = y1;
   this.y2 = y2;
 
-  console.log(x1);
 
   this.x1b = 0; 
   this.y1b = 0; 
@@ -54,48 +55,31 @@ var MovingLine = function(x1,x2,y1,y2){
 }
 
 MovingLine.prototype.update = function(){
-
-  var x1 = this.x1;
-  var y1 = this.y1;
-  var x2 = this.x2;
-  var y2 = this.y2;
-
-<<<<<<< HEAD
-  console.log(x1);
-
-=======
->>>>>>> master
-  var x1b = this.x1b; 
-  var y1b = this.y1b; 
-  var x2b = this.x2b; 
-  var y2b = this.y2b;
-
     
-  if(inc == 0){
-    x1b = random(50,200);
-    y1b = random(50,200);
-    x2b = random(20,320);
-    y2b = random(20, 200);
+  if(mouseIsPressed){
+    this.x1b = random(50,200);
+    this.y1b = random(50,200);
+    this.x2b = random(20,320);
+    this.y2b = random(20, 200);
+    inc = 0;
   }
 
 
-  x1 = lerp(x1,x1b,inc/100);
-  y1 = lerp(y1,y1b,inc/100);
-  x2 = lerp(x2,x2b,inc/100);
-  y2 = lerp(y2,y2b,inc/100);
+  this.x1 = lerp(this.x1,this.x1b,inc/400);
+  this.y1 = lerp(this.y1,this.y1b,inc/400);
+  this.x2 = lerp(this.x2,this.x2b,inc/400);
+  this.y2 = lerp(this.y2,this.y2b,inc/400);
+
+
 
 }
 
 MovingLine.prototype.draw = function(){
 
-  var x1 = this.x1;
-  var y1 = this.y1;
-  var x2 = this.x2;
-  var y2 = this.y2;
-
-  strokeWeight(5);
+  strokeWeight(4);
+  stroke(this.red, this.green, this.blue)
   //Draw a rectangle with parameters x,y,width,height
-  line(x1,y1,x2,y2);
+  line(this.x1,this.y1,this.x2,this.y2);
 
 }
 
